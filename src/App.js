4420ceb6238b224ea53dcbba76179e2a4baa6ae0
Camilo CV <cama0047@algonquinlive.com>
2021-05-24 01:15:42 -0400

    adding API KEY

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import React from 'react';
+import SearchMovies from "./SearchMovies.js";
 
 function App() {
+
   return (
     <div >
-      
+      <div className="container">
+        <h1 className="title">React Movie Finder</h1>
+        <SearchMovies/>
+      </div>
     </div>
   );
 }
